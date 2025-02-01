5ceb8a5d7dbd5c1311adcfd553ecd73426858a16
hackiku <ivan@pipewriter.io>
2025-02-01 14:41:09 +0100

    tested xml no undefined

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -2,45 +2,59 @@
 import { getProjects } from '$lib/server/contentful';
 import type { RequestHandler } from './$types';
 
-export const GET: RequestHandler = async () => {
-	const projects = await getProjects();
-	const today = new Date().toISOString().split('T')[0];
-	const baseUrl = 'https://hanaramujkic.com';
+export const prerender = true;
 
-	const xml = `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-            xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
-      <url>
+export const GET = (async () => {
+	const projects = await getProjects();
+	const baseUrl = 'https://hanaramujkic.com';
+	const today = new Date().toISOString().split('T')[0];
+
+	const slugMap = {
+		"Die Riesen vom Berge": "die-riesen-vom-berge",
+		"Kapitän Nemos Bibliothek": "kapitan-nemos-bibliothek",
+		"Dippel. Diagnose Cin 3": "dippel-diagnose-cin-3",
+		"Reigen": "reigen",
+		"Melancholia": "melancholia",
+		"Der Kopf der Ariadne!": "der-kopf-der-ariadne",
+		"Cinderella": "cinderella"
+	};
+
+	const sitemap = `<?xml version="1.0" encoding="UTF-8" ?>
+<urlset 
+    xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"
+    xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
+    <url>
         <loc>${baseUrl}</loc>
         <lastmod>${today}</lastmod>
-        <changefreq>monthly</changefreq>
+        <changefreq>weekly</changefreq>
         <priority>1.0</priority>
         <image:image>
-          <image:loc>${baseUrl}/hana-ramujkic.jpeg</image:loc>
-          <image:title>Hana Ramujkic - Set and Costume Designer</image:title>
+            <image:loc>${baseUrl}/hana-ramujkic.jpeg</image:loc>
         </image:image>
-      </url>
-      ${projects.map(project => `
-        <url>
-          <loc>${baseUrl}/${project.slug}</loc>
-          <lastmod>${today}</lastmod>
-          <changefreq>monthly</changefreq>
-          <priority>0.8</priority>
-          ${project.media.slice(0, 5).map(media => `
-            <image:image>
-              <image:loc>${media.url}</image:loc>
-              <image:title>${project.title} - Set Design by Hana Ramujkic</image:title>
-              <image:caption>${project.venue || 'Theater Production'}</image:caption>
-            </image:image>`
-	).join('')}
-        </url>
-      `).join('')}
-    </urlset>`.trim();
+    </url>
+    ${projects.map(project => {
+		const slug = slugMap[project.title] || project.title.toLowerCase().replace(/ /g, '-');
+		return `
+    <url>
+        <loc>${baseUrl}/${slug}</loc>
+        <lastmod>${today}</lastmod>
+        <changefreq>monthly</changefreq>
+        <priority>0.8</priority>
+        ${project.media.slice(0, 5).map(media => `
+        <image:image>
+            <image:loc>${media.url}</image:loc>
+        </image:image>`).join('')}
+    </url>`
+	}).join('')}
+</urlset>`.trim();
 
-	return new Response(xml, {
+	return new Response(sitemap, {
 		headers: {
-			'Content-Type': 'application/xml',
-			'Cache-Control': 'max-age=0, s-maxage=3600'
-		}
+			'Content-Type': 'application/xml;charset=UTF-8',
+			'Cache-Control': 'public, max-age=0, must-revalidate',
+			'X-Content-Type-Options': 'nosniff'
+		},
 	});
-};
+}) satisfies RequestHandler;

23afe7a57643b319698d176d9618cb5a51ec9d32
hanaramujkic <hanaramujkic1@gmail.com>
2026-04-29 14:17:25 +0200

    Delete src/src directory

--- src/src/lib/pages/landing/Contact.svelte
+++
@@ -1,8 +0,0 @@
-<!-- src/lib/pages/landing/Contact.svelte -->
-<script lang="ts">
-  import ContactForm from '$lib/components/cta/ContactForm.svelte';
-</script>
-
-<div class="container md:max-w-lg">
-	<ContactForm textSize="text-xl md:text-2xl" />
-</div>
--- src/src/routes/+page.svelte
+++
@@ -1,8 +0,0 @@
-<!-- src/routes/+page.svelte -->
-<script lang="ts">
-  import Landing from '$lib/pages/landing/Landing.svelte';
-  
-  export let data;
-</script>
-
-<Landing projects={data.projects} />

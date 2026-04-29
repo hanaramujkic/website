6ae76850b967b8ea952699a04e1436efba84d11f
hanaramujkic <hanaramujkic1@gmail.com>
2026-04-29 14:35:50 +0200

    Update Hero.svelte

--- src/lib/pages/landing/Hero.svelte
+++ src/lib/pages/landing/Hero.svelte
@@ -3,12 +3,11 @@
   import VideoPlayer from '$lib/components/VideoPlayer.svelte';
 </script>
 
-<section class="bg-black text-white">
-
+<section class="bg-white text-black dark:bg-black dark:text-white">
   <!-- Mobile + iPad / tablet -->
   <div class="xl:hidden">
-    <!-- TITLE -->
-    <div class="px-4 md:px-12 lg:px-16 pt-28 md:pt-32 pb-10">
+    <!-- TITLE AREA -->
+    <div class="px-4 md:px-12 lg:px-16 pt-28 md:pt-32 pb-10 bg-white dark:bg-black">
       <div class="text-right flex flex-col items-end gap-2">
         <h1 class="text-[2.2rem] md:text-[3.4rem] lg:text-[4rem] font-medium tracking-wide leading-[0.95]">
           SCENOGRAPHY
@@ -24,9 +23,9 @@
       </div>
     </div>
 
-    <!-- FULL WIDTH VIDEO -->
-    <div class="pb-2">
-      <div class="relative left-1/2 w-screen -translate-x-1/2 aspect-[21/9] overflow-hidden">
+    <!-- FULL WIDTH CINEMASCOPE VIDEO -->
+    <div class="bg-white dark:bg-black">
+      <div class="relative left-1/2 w-screen -translate-x-1/2 aspect-[21/9] overflow-hidden -mb-px">
         <VideoPlayer
           src="/die_riesen_vom_berge_hero.mp4"
           poster=""
@@ -37,7 +36,7 @@
   </div>
 
   <!-- Desktop -->
-  <div class="hidden xl:block relative w-full h-[80vh] overflow-hidden bg-black mb-32">
+  <div class="hidden xl:block relative w-full h-[80vh] overflow-hidden bg-black mb-16">
     <VideoPlayer
       src="/die_riesen_vom_berge_hero.mp4"
       poster=""
@@ -46,8 +45,6 @@
       <div class="absolute inset-0 bg-gradient-to-r from-black/35 via-black/10 to-transparent z-10"></div>
 
       <div class="absolute inset-0 z-20 px-24 2xl:px-32">
-
-        <!-- TITLE -->
         <div class="absolute top-32 left-[34%] -translate-x-1/2">
           <div class="text-white text-right flex flex-col items-end gap-2">
             <h1 class="text-[4.4rem] font-medium tracking-wide leading-none whitespace-nowrap">
@@ -63,9 +60,7 @@
             </h1>
           </div>
         </div>
-
       </div>
     </VideoPlayer>
   </div>
-
 </section>
